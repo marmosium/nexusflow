f87177aaa432f0c49d6a4497f03edb3d897ef9d5
Hüsnü Ercankaya <ercankayahusnu@gmail.com>
2026-05-19 22:11:20 +0300

    feat: 19.05.2026 BAŞLIYORUZ...

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-red-500 h-screen flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold">
+        19.05.2026 BAŞLIYORUZZZ !
+        <br />
+        HÜSNÜ
+        <br />
+        ERCANKAYA
+      </h1>
+    </div>
+  );
+}
+export default App;
