732f58e514aa0cb719f37580c29fef6bba1810b0
Hüsnü Ercankaya <ercankayahusnu@gmail.com>
2026-05-20 00:07:48 +0300

    Prepare NexusFlow app foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 function App() {
   return (
-    <div className="bg-red-500 h-screen flex items-center justify-center">
-      <h1 className="text-white text-5xl font-bold">
-        19.05.2026 BAŞLIYORUZZZ !
-        <br />
-        HÜSNÜ
-        <br />
-        ERCANKAYA
-      </h1>
+    <div className="bg-gray-900 h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">NexusFlow</h1>
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
